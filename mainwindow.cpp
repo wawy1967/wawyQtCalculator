0e00d2c0e06de18a9d2eda7bb724d7de8bc4f1e1
wawy1967 <wawy.engineer@gmail.com>
2018-04-13 19:01:00 +0800

    Implement the code. Change from hello to calcuulator.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,34 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButtonAdd_clicked()
+{
+    double c;
+    c = ui->a->value()+ui->b->value();
+    ui->lcdNumber->display(c);
+}
+
+void MainWindow::on_pushButtonSub_clicked()
+{
+    double c;
+    c = ui->a->value() - ui->b->value();
+    ui->lcdNumber->display(c);
+}
+
+void MainWindow::on_pushButtonMul_clicked()
+{
+    double c;
+    c = ui->a->value() * ui->b->value();
+    ui->lcdNumber->display(c);
+}
+
+void MainWindow::on_pushButtonDiv_clicked()
+{
+    double c;
+    if (ui->b->value() != 0.0)
+        c = ui->a->value()/ui->b->value();
+    else
+        c = 0.0;
+    ui->lcdNumber->display(c);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButtonAdd_clicked();
+
+    void on_pushButtonSub_clicked();
+
+    void on_pushButtonMul_clicked();
+
+    void on_pushButtonDiv_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
